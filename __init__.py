086b00c23166ce91a79ab8344c15510fee0efeee
PvBB <45363559+PvBB@users.noreply.github.com>
2018-11-27 16:20:14 +0100

    include audio ducking, first try

--- __init__.py
+++ __init__.py
@@ -1,7 +1,18 @@
+import re
+from mycroft.skills.core import intent_handler, intent_file_handler
+from mycroft.util.parse import match_one, fuzzy_match
+from mycroft.util.log import LOG
+from mycroft.api import DeviceApi
+from padatious import IntentContainer
+from requests import HTTPError
+from adapt.intent import IntentBuilder
+
+import time
+from subprocess import Popen, DEVNULL
+import signal
+from socket import gethostname
+import random
 from mycroft.skills.common_play_skill import CommonPlaySkill, CPSMatchLevel
-
-from mycroft.util.parse import match_one
-
 
 track_dict = {
     'sky radio': 'http://playerservices.streamtheworld.com/api/livestream-redirect/SKYRADIO.mp3',
@@ -10,7 +21,7 @@ track_dict = {
 }
 
 
-class TutorialSkill(CommonPlaySkill):
+class InternetRadioSkill(CommonPlaySkill):
     def CPS_match_query_phrase(self, phrase):
         """ This method responds wether the skill can play the input phrase.
 
@@ -42,7 +53,42 @@ class TutorialSkill(CommonPlaySkill):
         """
         url = data['track']
         self.audioservice.play(url)
+        
+    #### implement audio ducking
+    def handle_listener_started(self, message):
+        """ Handle auto ducking when listener is started.
+        The ducking is enabled/disabled using the skill settings on home.
+        TODO: Evaluate the Idle check logic
+        """
+        if self.audioservice.is_playing():
+            self.__pause()
+            self.ducking = True
+
+            # Start idle check
+            self.idle_count = 0
+            self.cancel_scheduled_event('IdleCheck')
+            self.schedule_repeating_event(self.check_for_idle, None,
+                                          1, name='IdleCheck')
+
+    def check_for_idle(self):
+        """ Repeating event checking for end of auto ducking. """
+        if not self.ducking:
+            self.cancel_scheduled_event('IdleCheck')
+            return
+
+        active = self.enclosure.display_manager.get_active()
+        if not active == '' or active == 'InternetRadioSkill':
+            # No activity, start to fall asleep
+            self.idle_count += 1
+
+            if self.idle_count >= 5:
+                # Resume playback after 5 seconds of being idle
+                self.cancel_scheduled_event('IdleCheck')
+                self.ducking = False
+                self.resume()
+        else:
+            self.idle_count = 0
 
 
 def create_skill():
-    return TutorialSkill()
+    return InternetRadioSkill()
